563a34b95e074e6041d322d3c3f48aef856bf655
Jonathan Bochard <jonathan.bochard@fr.clara.net>
2024-07-15 15:25:01 +0200

    fix(controller): roles

--- internal/controller/squidinstance_controller.go
+++ internal/controller/squidinstance_controller.go
@@ -51,7 +51,7 @@ type SquidInstanceReconciler struct {
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="",resources=service,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=networking,resources=ingress,verbs=get;list;watch;create;update;patch;delete
 
 //+kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch;delete
