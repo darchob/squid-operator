16dee05165e434faf0c966d7d7bb716a862bee6b
Jonathan Bochard <jonathan.bochard@fr.clara.net>
2024-07-15 15:08:05 +0200

    fix(controller): list services

--- internal/controller/squidconfigs_controller.go
+++ internal/controller/squidconfigs_controller.go
@@ -60,6 +60,7 @@ const (
 //+kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=deployment,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=service,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
