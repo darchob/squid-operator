6e3c0164dc545a462d4accb6b5aa3cd8970b286e
Jonathan Bochard <jonathan.bochard@fr.clara.net>
2024-07-16 10:22:45 +0200

    fix(storage): volume selector

--- pkg/squid/storages.go
+++ pkg/squid/storages.go
@@ -6,7 +6,6 @@ import (
 	squidv1 "git.fr.clara.net/claranet/healthcare/buildops/projects/kubernetes/operators/squid-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Init secondary configMap
@@ -23,12 +22,9 @@ func PersistentVolumeClaim(sr *squidv1.SquidInstance) *corev1.PersistentVolumeCl
 	return &corev1.PersistentVolumeClaim{
 		ObjectMeta: *ObjectMeta(sr),
 		Spec: corev1.PersistentVolumeClaimSpec{
-			// VolumeName: fmt.Sprintf("%s-volume", sr.Name),
+			VolumeName: fmt.Sprintf("%s-volume", sr.Name),
 			AccessModes: []corev1.PersistentVolumeAccessMode{
 				corev1.ReadWriteOnce,
-			},
-			Selector: &metav1.LabelSelector{
-				MatchLabels: Labels(sr),
 			},
 			StorageClassName: &sr.Spec.StorageClassName,
 			Resources: corev1.VolumeResourceRequirements{
