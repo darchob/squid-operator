ee9ef172555f40e7b06de6e42a48e9f011da2f75
jonathan bochard <jonathan.bochard@fr.clara.net>
2025-04-28 11:16:20 +0200

    fix(webhook): loop while waiting jobs status
    Changelogs: change

--- api/v1/squidconfigs_webhook.go
+++ api/v1/squidconfigs_webhook.go
@@ -170,6 +170,9 @@ func (r *SquidConfigs) waitForJobCompletion(ctx context.Context, jobName string)
 				Namespace: r.Namespace,
 				Name:      jobName,
 			}, &job); err != nil {
+				if client.IgnoreNotFound(err) != nil {
+					return false, err
+				}
 				return false, err
 			}
 
